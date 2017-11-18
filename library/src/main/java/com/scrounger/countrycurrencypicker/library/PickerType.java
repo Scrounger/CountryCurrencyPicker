0e12e266443851e23ccd1eacd560d8a4bd5e4f96
Scrounger <scrounger@gmx.net>
2017-11-18 08:52:52 +0100

    PickerType added

---
+++ library/src/main/java/com/scrounger/countrycurrencypicker/library/PickerType.java
@@ -0,0 +1,11 @@
+package com.scrounger.countrycurrencypicker.library;
+
+/**
+ * Created by ab on 18.11.2017.
+ */
+public enum PickerType {
+    COUNTRY,
+    COUNTRYandCURRENCY,
+    CURRENCY,
+    CURRENCYandCOUNTRY;
+}
